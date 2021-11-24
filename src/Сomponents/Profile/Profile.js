bb7c2df8006e897c650480de27725b976bf7fdf8
Fosa1990 <fosa1990@gmail.com>
2021-11-24 15:57:11 +0200

    Create Profile.js

---
+++ src/Сomponents/Profile/Profile.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+import Stats from './ProfileItem';
+
+const Profile = ({ username, tag, location, avatar }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img
+          src={avatar}
+          alt={username}
+          className={styles.avatar}
+          width="30px"
+          height="30px"
+        />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <Stats {...Stats} />
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  username: 'unknown user',
+  tag: 'no tag',
+  location: 'no location',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  followers: '0',
+  views: '0',
+  likes: '0',
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
+
+export default Profile;
