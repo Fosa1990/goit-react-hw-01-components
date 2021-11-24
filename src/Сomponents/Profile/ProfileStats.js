faf6100d0064331fc1859ef8a60dde57dee6bd51
Fosa1990 <fosa1990@gmail.com>
2021-11-24 16:12:30 +0200

    Create ProfileStats.js

---
+++ src/Сomponents/Profile/ProfileStats.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Profile.module.css';
+
+const ProfileItem = ({ followers, views, likes }) => {
+  return (
+    <ul className={styles.stats}>
+      <li>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{followers}</span>
+      </li>
+      <li>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{views}</span>
+      </li>
+      <li>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{likes}</span>
+      </li>
+    </ul>
+  );
+};
+
+export default ProfileItem;
