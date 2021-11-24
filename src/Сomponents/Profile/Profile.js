feac06d6cc74460efbd4373d8a81c38466358b39
Fosa1990 <fosa1990@gmail.com>
2021-11-24 16:12:17 +0200

    Update Profile.js

--- src/Сomponents/Profile/Profile.js
+++ src/Сomponents/Profile/Profile.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
-import Stats from './ProfileItem';
+import ProfileStats from './ProfileStats';
 
-const Profile = ({ username, tag, location, avatar }) => {
+const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
-        <img
-          src={avatar}
-          alt={username}
-          className={styles.avatar}
-          width="30px"
-          height="30px"
-        />
+        <img src={avatar} alt={username} className={styles.avatar} />
         <p className={styles.name}>{username}</p>
         <p className={styles.tag}>@{tag}</p>
         <p className={styles.location}>{location}</p>
       </div>
 
-      <Stats {...Stats} />
+      <ProfileStats {...stats} />
     </div>
   );
 };
