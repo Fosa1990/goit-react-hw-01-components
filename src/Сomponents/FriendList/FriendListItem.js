f186cf629fe3cbbf9a58d52dfe310928f76464ea
Fosa1990 <fosa1990@gmail.com>
2021-11-24 19:23:53 +0200

    Create FriendListItem.js

---
+++ src/Сomponents/FriendList/FriendListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
+
+const FriendListItem = ({ id, avatar, name, isOnline }) => {
+  console.log(id, avatar, name, isOnline);
+  return (
+    <li className={styles.item} key={id}>
+      <span
+        className={isOnline ? styles.statusOnline : styles.statusOffline}
+      ></span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+export default FriendListItem;
