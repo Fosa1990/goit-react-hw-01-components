187e8551d657e21a246dfbb855ea92936e3d9ba2
Fosa1990 <fosa1990@gmail.com>
2021-11-25 07:26:41 +0200

    changed the type of obtaining properties

--- src/Сomponents/Profile/Profile.js
+++ src/Сomponents/Profile/Profile.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 import ProfileStats from './ProfileStats';
 
-const Profile = ({ username, tag, location, avatar, stats }) => {
+const Profile = ({ user }) => {
+  const { username, tag, location, avatar, stats } = user;
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
@@ -29,15 +30,17 @@ Profile.defaultProps = {
 };
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }),
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
 };
 
 export default Profile;
