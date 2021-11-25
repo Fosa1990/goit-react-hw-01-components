6e41083f4e70c9707e0e8534579c04d7b3d8be14
Fosa1990 <fosa1990@gmail.com>
2021-11-25 06:57:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Section from './Сomponents/Section/Section';
 import Profile from './Сomponents/Profile/Profile';
 import Statistics from './Сomponents/Statistics/Statistics';
 import FriendList from './Сomponents/FriendList/FriendList';
+import TransactionHistory from './Сomponents/Transactions/TransactionHistory';
 import userData from './data/user.json';
 import statisticData from './data/statistics.json';
 import friendListData from './data/friends.json';
+import transactionData from './data/transactions.json';
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
       />
       <Statistics title="Upload stats" stats={statisticData} />
       <FriendList friendsData={friendListData} />
+      <TransactionHistory transactionData={transactionData} />
     </Section>
   );
 }
