3d65947fd8e202a38dd577c71d2ed2b4f5290c67
Fosa1990 <fosa1990@gmail.com>
2021-11-25 17:21:09 +0200

    renamed folder Components to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Section from './Сomponents/Section/Section';
-import Profile from './Сomponents/Profile/Profile';
-import Statistics from './Сomponents/Statistics/Statistics';
-import FriendList from './Сomponents/FriendList/FriendList';
-import TransactionHistory from './Сomponents/Transactions/TransactionHistory';
+import Section from './components/Section/Section';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/Transactions/TransactionHistory';
 import userData from './data/user.json';
 import statisticData from './data/statistics.json';
 import friendListData from './data/friends.json';
