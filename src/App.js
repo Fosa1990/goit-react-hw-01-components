dfa48fb7c238c6e5709996a1ac568972e62cd1ff
Fosa1990 <fosa1990@gmail.com>
2021-11-26 15:10:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import userData from './data/user.json';
 import statisticData from './data/statistics.json';
 import friendListData from './data/friends.json';
 import transactionData from './data/transactions.json';
+import colorsData from './data/colors.json';
 
 function App() {
   return (
     <Section classNames="userCard">
       <Profile user={userData} />
-      <Statistics title="Upload stats" stats={statisticData} />
+      <Statistics
+        title="Upload stats"
+        stats={statisticData}
+        colors={colorsData}
+      />
       <FriendList friendsData={friendListData} />
       <TransactionHistory transactionData={transactionData} />
     </Section>
