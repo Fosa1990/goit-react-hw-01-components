9a4e561a9695fd1b5455d2e44fe76050cff54187
Fosa1990 <fosa1990@gmail.com>
2021-11-30 10:14:39 +0200

    Update FriendList.js

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
 import styles from './FriendList.module.css';
@@ -6,13 +5,26 @@ import styles from './FriendList.module.css';
 const FriendList = ({ friendsData }) => {
   return (
     friendsData.length > 0 && (
-      <ul className={styles.friendList}>{friendsData.map(FriendListItem)}</ul>
+      <ul className={styles.friendList}>
+        {friendsData.map(({ id, avatar, name, isOnline }) => (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        ))}
+      </ul>
     )
   );
 };
 
 FriendList.propTypes = {
-  friendsData: PropTypes.arrayOf(PropTypes.shape().isRequired).isRequired,
+  friendsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
 
 export default FriendList;
